647035d9aa214fd2c1dd94e4d353fbc88fdd13bf
Lim jeong woo <dlawjddn0105@gmail.com>
2024-12-24 14:43:49 +0900

    initial commit

---
+++ src/test/java/com/example/stazcicd/StazCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stazcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StazCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
