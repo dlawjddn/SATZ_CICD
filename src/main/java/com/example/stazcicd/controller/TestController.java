2225fe6c7765d99d79120cf3f5f24638d9461cd3
Lim jeong woo <dlawjddn0105@gmail.com>
2025-02-24 12:34:06 +0900

    FEAT: 부하 테스트용 로직 추가

--- src/main/java/com/example/stazcicd/controller/TestController.java
+++ src/main/java/com/example/stazcicd/controller/TestController.java
@@ -12,6 +12,15 @@ public class TestController {
         return "on going!";
     }
 
+    @GetMapping("/stress")
+    public Integer stressTest() {
+        Integer result = 0;
+        for(int i=1; i<=100; i++) {
+            result += i;
+        }
+        return result;
+    }
+
     @GetMapping("/conf")
     public String checkDiffConf() {
         return "different nginx conf is working";
